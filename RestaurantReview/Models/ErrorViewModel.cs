06af143c9f7dc16258824b72661d12bcbac01496
Yuriy Yarytskyy <iamfromukraine@gmail.com>
2021-08-27 10:40:53 -0400

    addint initial files with gitignore

---
+++ RestaurantReview/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestaurantReview.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
