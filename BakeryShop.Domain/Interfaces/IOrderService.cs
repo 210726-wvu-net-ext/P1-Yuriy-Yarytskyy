880581c0d27519c265ccb70ad7503503dd00e016
Yuriy Yarytskyy <yyarytskyy@gmail.com>
2021-09-23 14:41:18 -0400

    Updated receipt and fixed some bugs

--- BakeryShop.Domain/Interfaces/IOrderService.cs
+++ BakeryShop.Domain/Interfaces/IOrderService.cs
@@ -13,10 +13,5 @@ namespace BakeryShop.Domain.Interfaces
         OrderModel GetOrderDetails(string OrderId);
         IEnumerable<Order> GetUserOrders(int UserId);
         int PlaceOrder(int userId, string orderId, string paymentId, CartModel cart, Address address);
-<<<<<<< HEAD
-        //PagingListModel<OrderModel> GetOrderList(int page = 1, int pageSize = 10);
-=======
-       
->>>>>>> 54966e0325f6b567a459831c54ee7e468a3fada7
     }
 }
